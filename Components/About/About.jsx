58ae5b25b1c0afe80857350e2979e64863353fba
Saloni-FED <salonipandey014@gmail.com>
2024-07-17 12:03:55 +0530

    Image size change

--- Components/About/About.jsx
+++ Components/About/About.jsx
@@ -10,7 +10,7 @@ const AboutUs = () => {
           <p className="mb-6 font-sora font-semibold">Welcome to Aistear Unica—your premier destination for exceptional Makeup Services delivered right to your doorstep. Founded in 2023 by Ms. Sarika, a seasoned entrepreneur with over 15 years in the Beauty and Makeup industry, Aistear Unica is more than just a service; it's a movement towards empowering clients with confidence and elegance, regardless of the occasion.</p>
           <p className="mb-6 font-sora font-semibold">"Aistear Unica" merges the essence of two languages—Irish and Latin—to craft a name that resonates with uniqueness and exploration. 'Aistear' means 'journey' or 'adventure' in Irish, while 'Unica' translates to 'unique' or 'only one' in Latin. This fusion symbolizes a 'Unique Journey' or a 'One-of-a-kind Adventure,' embodying our mission to deliver a distinct and tailored experience. At Aistear Unica, we see every customer's journey as uniquely their own. Our commitment to innovation and personalization is woven into our name, ensuring that our solutions not only meet your individual needs but celebrate them. Embark on your personalized adventure with Aistear Unica, where innovation meets individuality.</p>
         </div>
-        <div className="lg:w-[25%] mt-10 lg:mt-0 lg:ml-10">
+        <div className="w-[25%] mt-10 lg:mt-0 lg:ml-10">
           <img src="/images/img.png" alt="Makeup Service" className="rounded-lg shadow-lg w-full"/>
         </div>
       </div>
