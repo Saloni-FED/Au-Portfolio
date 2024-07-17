cfb8f24907dd9d4632cd076c5341d5a27adab36b
Saloni-FED <salonipandey014@gmail.com>
2024-07-17 09:52:01 +0530

    changes made

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import About from "@/Components/About/About";
 const page = () => {
   return (
-    <div className=" pt-40 text-white mx-auto container ">
+    <div className=" pt-40 text-white mx-auto container bg-black">
       <img src="/images/About(BLUR).png" className=" mx-auto w-64 sc:block hidden"/>
       <div>
         <About/>
