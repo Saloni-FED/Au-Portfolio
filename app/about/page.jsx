59ea92d8462022deb6b49a3a53d45d99088097db
Saloni-FED <salonipandey014@gmail.com>
2024-07-17 12:18:32 +0530

    Gap decreased

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,7 +3,7 @@ import About from "@/Components/About/About";
 import Stats from "@/Components/About/Stats";
 const page = () => {
   return (
-    <div className="pt-36 text-white mx-auto container bg-black">
+    <div className="pt-32 text-white mx-auto container bg-black">
       <img src="/images/About(BLUR).png" className=" mx-auto w-64 sc:block hidden"/>
       <div>
         <About/>
