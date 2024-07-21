66526ead37b329ba4cad033fb206e344eb80b9ab
Saloni-FED <salonipandey014@gmail.com>
2024-07-21 09:22:21 +0530

    carousel created

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-full pt-32 bg-white h-screen">
-      Hello about us
+      Home Page
     </div>
   )}
       
