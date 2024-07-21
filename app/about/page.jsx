e7b98648bfead1bb4b9951b9be017bbe77a13d5b
Saloni-FED <salonipandey014@gmail.com>
2024-07-21 09:29:12 +0530

    meta added

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -7,34 +7,35 @@ import WhatWeDo from "@/Components/About/WhatWeDo";
 import OurCommitment from "@/Components/About/OurCommitment";
 import WhyChooseUs from "@/Components/About/WhyChooseUs";
 import ContactForm from "@/Components/About/ContactForm";
-import Head from "next/head";
+// import { metadata } from "../layout";
 // import FounderProfile from "@/Components/About/FounderProfile";
-
 export const metadata = {
-  title: "About Aistear Unica - Empowering Artists & Connecting You with Creativity",
-  description: "Learn about Aistear Unica's mission to empower artists & connect you with exceptional makeup artists & artistic brilliance in India. Discover our story & values.",
-  keywords: "Aistear Unica about us, makeup artist India, marketplace for makeup artist"
+  title:
+    "About Aistear Unica - Empowering Artists & Connecting You with Creativity",
+  description:
+    " Learn about Aistear Unica's mission to empower artists & connect you with exceptional makeup artists & artistic brilliance in India. Discover our story & values.",
+
+  keywords:
+    "makeup artist India, bridal makeup artist India, aistear unica, makeup for weddings, hire makeup artists",
 };
 const page = () => {
   return (
-    <>
-      <div className="md:pt-32 py-28 text-white mx-auto container bg-black">
-        <img
-          src="/images/About(BLUR).png"
-          className=" mx-auto w-64 sc:block hidden"
-        />
-        <div>
-          <About />
-          <Stats />
-          <Founder />
-          <Mission />
-          <WhatWeDo />
-          <OurCommitment />
-          <WhyChooseUs />
-          <ContactForm />
-        </div>
+    <div className="md:pt-32 py-28 text-white mx-auto container bg-black">
+      <img
+        src="/images/About(BLUR).png"
+        className=" mx-auto w-64 sc:block hidden"
+      />
+      <div>
+        <About />
+        <Stats />
+        <Founder />
+        <Mission />
+        <WhatWeDo />
+        <OurCommitment />
+        <WhyChooseUs />
+        <ContactForm />
       </div>
-    </>
+    </div>
   );
 };
 
